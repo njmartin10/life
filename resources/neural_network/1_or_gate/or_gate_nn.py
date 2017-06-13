e18ddcb047380d06aeefa0b3e98343bf831bee26
Nathan Martin <njmartin10@gmail.com>
2017-06-12 21:33:18 -0700

    Pass truth table of tests

--- resources/neural_network/1_or_gate/or_gate_nn.py
+++ resources/neural_network/1_or_gate/or_gate_nn.py
@@ -10,7 +10,7 @@ import unittest
 #           0 otherwise
 #         }
 def or_gate(a, b):
-    return a
+    return (a or b)
 
 # Here's our "unit tests".
 class or_gate_tests(unittest.TestCase):
@@ -20,6 +20,12 @@ class or_gate_tests(unittest.TestCase):
 
     def test_both_true(self):
         self.assertTrue(or_gate(1,1))
+
+    def test_A_true(self):
+        self.assertTrue(or_gate(0,1))
+
+    def test_B_true(self):
+        self.assertTrue(or_gate(1,0))
 
 
 def main():
