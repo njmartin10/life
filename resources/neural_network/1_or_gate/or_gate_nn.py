0943e09d665c26aee50fa73cef845de3a4f49df5
Nathan Martin <njmartin10@gmail.com>
2017-06-13 18:54:18 -0700

    Initial functionality with no training

--- resources/neural_network/1_or_gate/or_gate_nn.py
+++ resources/neural_network/1_or_gate/or_gate_nn.py
@@ -15,16 +15,37 @@ def or_gate(x):
         y += i
     return (y > 0)
 
+def update_or_gate_weights(w):
+    w[0]
+
 # Here's our "unit tests".
 class two_input_or_gate_tests(unittest.TestCase):
 
     def test_both_false(self):
+        # set up expectations
         x = [0,0]
+        w = [0,0]
         self.assertFalse(or_gate(x))
 
+        # call function under test
+        update_or_gate_weights(w)
+
+        # Test that weights are not updated
+        self.assertFalse(w[0])
+        self.assertFalse(w[1])
+
     def test_both_true(self):
+        # set up expectations
         x = [1,1]
+        w = [0,0]
         self.assertTrue(or_gate(x))
+
+        # call function under test
+        update_or_gate_weights(w)
+
+        # Test that weights are updated
+        self.assertFalse(w[0])
+        self.assertFalse(w[1])
 
     def test_A_true(self):
         x = [0,1]
@@ -34,9 +55,13 @@ class two_input_or_gate_tests(unittest.TestCase):
         x = [1,0]
         self.assertTrue(or_gate(x))
 
-    def test_weights(self):
-        self.fail("Dont forget about me!")
+    def test_3_true_inputs(self):
+        x = [1,1,1]
+        self.assertTrue(or_gate(x))
 
+    def test_3_true_1_false_inputs(self):
+        x = [1,1,0,1]
+        self.assertTrue(or_gate(x))
 
 
 def main():
